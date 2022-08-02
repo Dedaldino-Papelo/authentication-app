3df9ca2813e24dfdfc24440429de0fcd5e0d450e
Dedalino <kelsonpapelo18@gmail.com>
2022-08-02 16:50:38 +0100

    adding static files from frontend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ const app = express()
 const User = require('./Routes/User')
 const cors = require("cors")
 require('dotenv').config()
+const path = require('path')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(User)
+
+//=====================================
+
+    app.use(express.static(path.join(__dirname,'dist')))
+    
+    app.get("*", (req,res) => {
+      res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    }) 
+  
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
